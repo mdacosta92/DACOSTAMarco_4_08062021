ce00bb96f9da715c1e191483c07330e0bc5acad6
mdacosta92 <79317416+mdacosta92@users.noreply.github.com>
2021-08-13 15:11:17 +0200

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -249,7 +249,7 @@ if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requir
                 if (this.$element.trigger(f), !f.isDefaultPrevented()) {
                     e && e.length && (c.call(e, "hide"), b || e.data("bs.collapse", null));
                     var g = this.dimension();
-                    this.$element.removeClass("collapse").addClass("collapsing")[g](0).attr("aria-expanded", !0), this.$trigger.removeClass("collapsed").attr("aria-expanded", !0), this.transitioning = 1;
+                    this.$element.removeClass("collapse").addClass("collapsing")[g](0).attr("aria-expanded", !0), this.$trigger.removeClass("collapse").attr("aria-expanded", !0), this.transitioning = 1;
                     var h = function() {
                         this.$element.removeClass("collapsing").addClass("collapse in")[g](""), this.transitioning = 0, this.$element.trigger("shown.bs.collapse")
                     };
